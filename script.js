a402946eb3c17f60c071e1c947cd28546dd5b2e0
A R Khan BappY <macbook@Macbook-Pro-Rahmyeda.local>
2024-11-09 23:38:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelector('.slides');
+  const totalSlides = document.querySelectorAll('.slide').length;
+  
+  if (index >= totalSlides) currentIndex = 0;
+  else if (index < 0) currentIndex = totalSlides - 1;
+  else currentIndex = index;
+  
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function moveSlide(step) {
+  showSlide(currentIndex + step);
+}
+
+setInterval(() => moveSlide(1), 5000);
